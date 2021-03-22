2a24f75f67367c92b66030c93a126629dbd53e01
Manuel Casocavallo <manuelcasocavallo@gmail.com>
2021-03-22 16:09:33 +0100

    Initial Commit

---
+++ HWS-2-Guess the Flag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HWS-2-Guess the Flag
+//
+//  Created by Manuel Casocavallo on 22/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HWS-2-Guess the Flag/HWS_2_Guess_the_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWS_2_Guess_the_FlagApp.swift
+//  HWS-2-Guess the Flag
+//
+//  Created by Manuel Casocavallo on 22/03/21.
+//
+
+import SwiftUI
+
+@main
+struct HWS_2_Guess_the_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
